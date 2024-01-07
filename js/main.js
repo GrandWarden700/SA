6bbe90f512cd71ef1d53efd58f18900165d71b51
aa870111 <asz18369@gmail.com>
2024-01-07 19:02:05 +0800

    chart

--- js/main.js
+++ js/main.js
@@ -64,19 +64,19 @@
     var myChart1 = new Chart(ctx1, {
         type: "bar",
         data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            labels: ["上午8:30", "下午12:00", "下午13:30", "下午15:00", "下午18:00", "下午20:30", "下午23:00"],
             datasets: [{
-                    label: "USA",
+                    label: "第一停車場",
                     data: [15, 30, 55, 65, 60, 80, 95],
                     backgroundColor: "rgba(0, 156, 255, .7)"
                 },
                 {
-                    label: "UK",
+                    label: "第二停車場",
                     data: [8, 35, 40, 60, 70, 55, 75],
                     backgroundColor: "rgba(0, 156, 255, .5)"
                 },
                 {
-                    label: "AU",
+                    label: "第三停車場",
                     data: [12, 25, 45, 55, 65, 70, 60],
                     backgroundColor: "rgba(0, 156, 255, .3)"
                 }
@@ -91,26 +91,27 @@
     // Salse & Revenue Chart
     var ctx2 = $("#salse-revenue").get(0).getContext("2d");
     var myChart2 = new Chart(ctx2, {
-        type: "line",
-        data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-            datasets: [{
-                    label: "Salse",
-                    data: [15, 30, 55, 45, 70, 65, 85],
-                    backgroundColor: "rgba(0, 156, 255, .5)",
-                    fill: true
-                },
-                {
-                    label: "Revenue",
-                    data: [99, 135, 170, 130, 190, 180, 270],
-                    backgroundColor: "rgba(0, 156, 255, .3)",
-                    fill: true
-                }
-            ]
-            },
-        options: {
-            responsive: true
-        }
+      type: 'line',
+      data: {
+        labels: ['上午8:30', '下午12:00', '下午13:30', '下午15:00', '下午18:00', '下午20:30', '下午23:00'],
+        datasets: [
+          {
+            label: '離峰預期車位',
+            data: [250, 80, 178, 222, 30, 125, 50],
+            backgroundColor: 'rgba(0, 156, 255, .5)',
+            fill: true,
+          },
+          {
+            label: '尖峰預期車位',
+            data: [50, 230, 122, 78, 270, 175, 250],
+            backgroundColor: 'rgba(0, 156, 255, .3)',
+            fill: true,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+      },
     });
     
 
